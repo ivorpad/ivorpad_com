541066e5f305913363b12aeb18a7dc47caf7fbb7
ivorpad <ivorjpc@gmail.com>
2018-09-25 17:33:08 -0400

    Testing styled components SRR

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react'
 import Layout from '../components/layout'
 import { graphql, Link } from 'gatsby'
+import styled from 'styled-components'
+
+const WorkSection = styled.div`
+  margin-top: 30px;
+`
 
 export default class WorkPage extends Component {
-
   constructor(props) {
     super(props)
 
     this.state = {
-       data: props.data.allContentfulWork.edges
+      data: props.data.allContentfulWork.edges,
     }
   }
 
   render() {
     return (
-    <Layout>
-      <div className="content grid">
-        {this.state.data.map(({ node: post }) => {
-          return <div key={ post.id } className="work__item">             
-              <h3 className="work__title">
-                <Link to={`/work/${post.slug}`} state={ {isInModal: true} } >
-                  {post.title.title}
-                </Link>
-              </h3>
-              <img src={post.featuredImage.resize.src} alt="" />
-            </div>
-        })}
-      </div>
-    </Layout>
+      <Layout>
+        <WorkSection>
+          <div className="content grid">
+            {this.state.data.map(({ node: post }) => {
+              return (
+                <div key={post.id} className="work__item">
+                  <img src={post.featuredImage.resize.src} alt="" />
+                  <h3 className="work__title">
+                    <Link to={`/work/${post.slug}`} state={{ isInModal: true }}>
+                      {post.title.title}
+                    </Link>
+                  </h3>
+                </div>
+              )
+            })}
+          </div>
+        </WorkSection>
+      </Layout>
     )
   }
 }
