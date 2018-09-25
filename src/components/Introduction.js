4dd128ddaeba79b1e52ecf1a06aeb5400f1550df
ivorpad <ivorjpc@gmail.com>
2018-09-25 17:49:55 -0400

    improving work section

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,28 +1,29 @@
 import React from 'react'
 
-const Introduction = ({data}) => {
-    const {
-      contentfulIntroduction,
-      contentfulAuthor
-    } = data
-    const {
-    	title,
-    	featuredImage,
-    	content
-    } = contentfulIntroduction;
+const Introduction = ({ data }) => {
+  const { contentfulIntroduction, contentfulAuthor } = data
+  const { title, featuredImage, content } = contentfulIntroduction
 
-    const {
-      name
-    } = contentfulAuthor
+  const { name } = contentfulAuthor
 
-  return <div className="content intro">
+  return (
+    <div className="content intro">
       <div className="intro__card">
-        <img className="intro__avatar" src={`${featuredImage.resize.src}`} alt="Ivor Padilla" />
-      <h1 className="intro__title">{title} <span>{name}</span></h1>
-        <div className="intro__content" dangerouslySetInnerHTML={{ __html: content.childMarkdownRemark.html }} />
+        <img
+          className="intro__avatar"
+          src={`${featuredImage.resize.src}`}
+          alt="Avatar of Web Developer: Ivor Padilla"
+        />
+        <h1 className="intro__title">
+          {title} <span>{name}</span>
+        </h1>
+        <div
+          className="intro__content"
+          dangerouslySetInnerHTML={{ __html: content.childMarkdownRemark.html }}
+        />
       </div>
     </div>
+  )
 }
 
-export default Introduction;
-
+export default Introduction
